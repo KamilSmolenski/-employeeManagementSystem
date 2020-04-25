9a9ae3f936564e2061bea934bf3f527e36cbdcdb
kamilsmolen.ski <smolenskikamil@gmail.com>
2020-04-25 11:11:36 +0200

    initial commit

---
+++ src/com/company/Employee.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Employee {
+    String firstName;
+    String lastName;
+    int seniority;
+    double salary;
+
+    public enum Position{
+        JUNIOR_DEVELOPER, DEVELOPER,SENIOR_DEVELOPER, SCRUM_MASTER, PRODUCT_OWNER, PROJECT_MANAGER;
+    }
+
+
+}
