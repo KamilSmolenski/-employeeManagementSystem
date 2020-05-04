c36267cb70351fe3de746c3dd5284c25f148780c
kamilsmolen.ski <smolenskikamil@gmail.com>
2020-05-04 12:06:15 +0200

    add getSeniority method

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -1,13 +1,44 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Employee {
+
+
     String firstName;
     String lastName;
+    String startWorkDate;
     int seniority;
     double salary;
+    Position position;
+
+    public Employee(String firstName, String lastName,   String startWorkDate, double salary, Position position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.startWorkDate = startWorkDate;
+        this.salary = salary;
+        this.position = position;
+    }
+
+     String getSeniorityinYears() throws ParseException {
+        SimpleDateFormat myDateFormat= new SimpleDateFormat("dd-MM-yyyy");
+
+        long startWorkDateStamp = myDateFormat.parse(this.startWorkDate).getTime();
+         long nowDate = new Date().getTime();
+
+         long seniority = (nowDate - startWorkDateStamp)/1000/60/60/24/30/12;
+
+         if(seniority<1){return "<1";}
+         else{
+            return ""+ seniority;}
+
+     }
 
     public enum Position{
-        JUNIOR_DEVELOPER, DEVELOPER,SENIOR_DEVELOPER, SCRUM_MASTER, PRODUCT_OWNER, PROJECT_MANAGER;
+        JUNIOR_DEVELOPER, DEVELOPER,SENIOR_DEVELOPER, SCRUM_MASTER, PRODUCT_OWNER,SECOND_PROJECT_MANAGER, PROJECT_MANAGER;
     }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+
+
+    public static void main(String[] args) throws ParseException {
+
+
+
+        Employee no1 = new Employee("Jan","Kowalski","27-03-2020",10000, Employee.Position.JUNIOR_DEVELOPER);
+        Employee no2 = new Employee("Adam","Miauczyński","15-07-1991",2222, Employee.Position.SECOND_PROJECT_MANAGER);
+        System.out.println(no1.getSeniorityinYears());
+        System.out.println(no2.getSeniorityinYears());
+
+
+}};
