173165072280a379fa4a796de223ebd1ea63aeaf
kamilsmolen.ski <smolenskikamil@gmail.com>
2020-05-05 13:22:45 +0200

    add JTable component

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -6,16 +6,31 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Employee {
+    String getSeniorityinYears() throws ParseException {
 
+        SimpleDateFormat myDateFormat= new SimpleDateFormat("dd-MM-yyyy");
+
+        long startWorkDateStamp = myDateFormat.parse(this.startWorkDate).getTime();
+        long nowDate = new Date().getTime();
+
+        long seniorityStampToYears = (nowDate - startWorkDateStamp)/1000/60/60/24/30/12;
+
+
+
+        if(seniorityStampToYears<1){return "<1";}
+        else{
+            return ""+ seniorityStampToYears;}
+
+    }
 
     String firstName;
     String lastName;
     String startWorkDate;
-    int seniority;
+    String seniority;
     double salary;
     Position position;
 
-    public Employee(String firstName, String lastName,   String startWorkDate, double salary, Position position) {
+    public Employee(String firstName, String lastName,   String startWorkDate, double salary, Position position) throws ParseException {
         this.firstName = firstName;
         this.lastName = lastName;
         this.startWorkDate = startWorkDate;
@@ -23,23 +38,51 @@ public class Employee {
         this.position = position;
     }
 
-     String getSeniorityinYears() throws ParseException {
-        SimpleDateFormat myDateFormat= new SimpleDateFormat("dd-MM-yyyy");
 
-        long startWorkDateStamp = myDateFormat.parse(this.startWorkDate).getTime();
-         long nowDate = new Date().getTime();
-
-         long seniority = (nowDate - startWorkDateStamp)/1000/60/60/24/30/12;
-
-         if(seniority<1){return "<1";}
-         else{
-            return ""+ seniority;}
-
-     }
 
     public enum Position{
         JUNIOR_DEVELOPER, DEVELOPER,SENIOR_DEVELOPER, SCRUM_MASTER, PRODUCT_OWNER,SECOND_PROJECT_MANAGER, PROJECT_MANAGER;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStartWorkDate() {
+        return startWorkDate;
+    }
+
+    public void setStartWorkDate(String startWorkDate) {
+        this.startWorkDate = startWorkDate;
+    }
+
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,120 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
-public class Main {
-
-
-
-    public static void main(String[] args) throws ParseException {
+public class Main extends JFrame {
 
 
+        public Main()   {
 
-        Employee no1 = new Employee("Jan","Kowalski","27-03-2020",10000, Employee.Position.JUNIOR_DEVELOPER);
-        Employee no2 = new Employee("Adam","Miauczyński","15-07-1991",2222, Employee.Position.SECOND_PROJECT_MANAGER);
-        System.out.println(no1.getSeniorityinYears());
-        System.out.println(no2.getSeniorityinYears());
+            initComponents();
+    }
+    public static void initComponents()  {
+          JFrame mainFrame = new JFrame("Lista Pracowników");
+          Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+          mainFrame.setSize(screenDim.width/2,screenDim.height/2);
+          mainFrame.setLocation(screenDim.width/4,screenDim.height/4);
+          mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          mainFrame.setVisible(true);
+
+        String[] columns = {"Imię", "Nazwisko","Staż pracy", "Wynagrodzenie","Stanowisko"};
+        Object[][] items ={
+                {"Jan","Kowalski","2","5000","Junior"},
+                {"Jan","Kowalski","4","3000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","Kowalski","5","7000","Junior"},
+                {"Jan","KoZA","5","7000","Junior"},
+
+        };
+
+
+
+        JTable table = new JTable(items,columns);
+        table.setEnabled(false);
+
+        JScrollPane  scrollPane = new JScrollPane(table);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS) ;
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(table.getTableHeader(),BorderLayout.CENTER);
+//        panel.add(table,BorderLayout.CENTER);
+        panel.add(scrollPane,BorderLayout.CENTER);
+
+        mainFrame.setContentPane(panel);
+
+        mainFrame.pack();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args)  {
+            new Main();
+
+
+
+
+
+
+
+//
+//        ITERATOR WYSWIETLAJACY PO KOLEJI NP IMIONA
+//
+//        Iterator<Employee> employeeIterator = listofEmployers.iterator();
+//        while (employeeIterator.hasNext()){
+//            System.out.println(employeeIterator.next().getFirstName());
+//        }
+            ///
+        //// to Samo tylko, że szybciej
+//        for(Employee e :listofEmployers){
+//            System.out.println(e.getLastName());
+//        }
 
 
 }};
