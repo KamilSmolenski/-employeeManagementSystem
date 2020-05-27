4136d69627fc6b751e14b5304eda49477734f919
kamilsmolen.ski <smolenskikamil@gmail.com>
2020-05-27 07:53:08 +0200

    add more features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.Comparator;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -18,29 +18,15 @@ public class Main {
 
 
         String[] headers = {"Imię", "Nazwisko", "Staż pracy","Wynagrodzenie","Stanowisko"};
-        Object[][] workers = {{"Ana", "Smith", Integer.valueOf(19),Integer.valueOf(2000),"paź"},
-                {"Alex", "Jones", Integer.valueOf(25),Integer.valueOf(223000),"paź"},
-                {"Peggy", "Carter", Integer.valueOf(32),Integer.valueOf(2994000),"paź"},
-                {"Pgy", "Cter", Integer.valueOf(32),Integer.valueOf(2990),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(12),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(124),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(126),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(222),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(212222),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29300),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(23),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(233900),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(2000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(24000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(29324000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(4000),"paź"},
-                {"Pey", "Car", Integer.valueOf(32),Integer.valueOf(500),"paź"},
+        Object[][] workers = {{"Ana", "Smith", Integer.valueOf(5),Integer.valueOf(10000),Position.MANAGER},
+                {"Adam", "Miauczyński", Integer.valueOf(20),Integer.valueOf(2200),Position.SECOND_MANAGER},
+                {"Jan", "Adam", Integer.valueOf(3),Integer.valueOf(6666),Position.PRODUCT_OWNER},
+                {"Karol", "Zięba", Integer.valueOf(5),Integer.valueOf(8000),Position.SCRUM_MASTER},
+                {"Jerzy", "Rutkowski", Integer.valueOf(1),Integer.valueOf(2300),Position.INTERN},
+                {"Zenon", "Małysz", Integer.valueOf(15),Integer.valueOf(8500),Position.SENIOR_DEVELOPER},
+                {"Wiktor", "Kowalski", Integer.valueOf(9),Integer.valueOf(6500),Position.DEVELOPER},
+                {"Jakub" , "Nowak", Integer.valueOf(2),Integer.valueOf(4500),Position.JUNIOR_DEVELOPER},
+
         };
 
 
@@ -55,14 +41,22 @@ public class Main {
 
 
         TableRowSorter<TableModel> sorter = new TableRowSorter<>(table.getModel());
-        int column = 3;
-        sorter.setComparator(column, new Comparator<Integer>() {
+
+        sorter.setComparator(3, new Comparator<Integer>() {
             @Override
             public int compare(Integer arg0, Integer arg1) {
                 return arg0.compareTo(arg1);
             }
         });
         table.setRowSorter(sorter);
+        sorter.setComparator(2, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer arg0, Integer arg1) {
+                return arg0.compareTo(arg1);
+            }
+        });
+        table.setRowSorter(sorter);
+
 
         JButton btnAdd = new JButton("Nowy pracownik");
         JButton btnEdit = new JButton("Edytuj tabelę");
@@ -240,6 +234,195 @@ public class Main {
 
 
 
+        });
+
+        btnSearch.addActionListener(e -> {
+            JFrame searchFrame = new JFrame("Wyszukaj");
+            Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+            searchFrame.setSize(screenDim.width / 5, screenDim.height / 10);
+            searchFrame.setLocation(screenDim.width / 4, screenDim.height / 4);
+            searchFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            searchFrame.setVisible(true);
+
+            JTextField searchField = new JTextField(15);
+            JButton searchBtn = new JButton("Szukaj");
+            JPanel searchPanel = new JPanel();
+            searchPanel.add(searchField);
+            searchPanel.add(searchBtn);
+            searchFrame.getContentPane().add(searchPanel,BorderLayout.CENTER);
+
+            searchBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    searchField.getDocument().addDocumentListener(new DocumentListener() {
+                        @Override
+                        public void insertUpdate(DocumentEvent e) {
+                            search(searchField.getText());
+                        }
+
+                        @Override
+                        public void removeUpdate(DocumentEvent e) {
+                            search(searchField.getText());
+                        }
+
+                        @Override
+                        public void changedUpdate(DocumentEvent e) {
+                            search(searchField.getText());
+                        }
+                        public void search(String str) {
+                            if (str.length() == 0) {
+                                sorter.setRowFilter(null);
+                            } else {
+                                sorter.setRowFilter(RowFilter.regexFilter(str));
+                            }
+                        }
+                    });
+                }
+            });
+
+        });
+
+        btnFilter.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame filteringFrame = new JFrame("Filtruj po pensji");
+                Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+                filteringFrame.setSize(screenDim.width / 5, screenDim.height / 4);
+                filteringFrame.setLocation(screenDim.width / 4, screenDim.height / 4);
+                filteringFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                filteringFrame.setVisible(true);
+
+                JRadioButton greaterThan = new JRadioButton();
+                greaterThan.setText("ponad");
+
+                JRadioButton lessThan  = new JRadioButton();
+                lessThan.setText("poniżej");
+                lessThan.isSelected();
+
+                ButtonGroup G = new ButtonGroup();
+                lessThan.setFont(new Font("SansSerif",Font.ITALIC,18));
+                greaterThan.setFont(new Font("SansSerif",Font.ITALIC,18));
+                G.add(greaterThan);
+                G.add(lessThan);
+
+                JSlider slider = new JSlider(JSlider.HORIZONTAL,0,10000,5);
+                slider.setPaintLabels(true);
+                Dimension d = slider.getPreferredSize();
+                slider.setPreferredSize(new Dimension(d.width + 100, d.height));
+
+                JTextField sliderValue = new JTextField(""+ slider.getValue(),5);
+                sliderValue.setFont(new Font("SansSerif",Font.BOLD,23));
+                sliderValue.setEditable(true);
+                sliderValue.setSize(200,10);
+                slider.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+                        sliderValue.setText(""+((JSlider) e.getSource()).getValue());
+                    }
+                });
+
+                JButton filterBtn = new JButton("Filtruj");
+
+
+
+                JPanel filterPanel = new JPanel();
+                JPanel filterPanel2 = new JPanel();
+                JPanel filterPanel3 = new JPanel();
+
+                filterPanel.add(greaterThan);
+                filterPanel.add(lessThan);
+                filterPanel2.add(slider);
+                filterPanel2.add(sliderValue);
+                filterPanel3.add(filterBtn);
+
+                filteringFrame.getContentPane().add(filterPanel,BorderLayout.BEFORE_FIRST_LINE);
+                filteringFrame.getContentPane().add(filterPanel2,BorderLayout.CENTER);
+                filteringFrame.getContentPane().add(filterPanel3,BorderLayout.AFTER_LAST_LINE);
+
+
+                    filterBtn.addActionListener(new ActionListener() {
+
+                        boolean filter = true;
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            Number number = Integer.valueOf(sliderValue.getText());
+                            int column = 3;
+
+                            if (greaterThan.isSelected()){
+                                if(filter)
+
+                                    sorter.setRowFilter(RowFilter.numberFilter(
+                                        RowFilter.ComparisonType.AFTER,number,column
+
+                                ));
+                            else
+                                sorter.setRowFilter(null);
+                                filter=!filter;
+
+
+                            } else if (lessThan.isSelected()){
+                                if(filter)
+                                    sorter.setRowFilter(RowFilter.numberFilter(
+                                            RowFilter.ComparisonType.BEFORE,number,column
+                                    ));
+                                else
+                                    sorter.setRowFilter(null);
+                                filter=!filter;
+
+
+                            }else if(!greaterThan.isSelected() && !lessThan.isSelected()){
+                                JOptionPane.showMessageDialog(table,"Nie zaznaczono ponad/poniżej","Określ kryteria filtrowania", JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                            if (filter) {
+                                filterBtn.setText("Filtruj");
+                            } else {
+                                filterBtn.setText("Cofnij filtr");
+                            }
+
+
+                        }
+                    });
+
+
+
+            }
+        });
+
+        btnSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                File file = new File("listaPracowników.txt");
+                PrintWriter writer = null;
+                try {
+                    writer = new PrintWriter(file);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                for(int row = 0; row < table.getRowCount(); row++) {
+
+                    for(int column = 0; column < table.getColumnCount(); column++) {
+                        writer.print(table.getColumnName(column) + ": ");
+                        writer.println(table.getValueAt(row, column));
+                    }
+                    writer.println(""); // Add line space
+                }
+                writer.close();
+
+                JOptionPane.showMessageDialog(table,"Lista została zapisana w: " + file.getAbsolutePath(),"Zapisano", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+
+        });
+
+        btnLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+
+            }
         });
 
         JFrame frame = new JFrame("Lista Pracowników");
